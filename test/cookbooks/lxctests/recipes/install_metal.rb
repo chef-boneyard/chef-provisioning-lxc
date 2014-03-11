33c271ad8700d1fbd9d622b2dc35a4e5a7a82899
John Keiser <jkeiser@opscode.com>
2014-03-10 19:18:58 -0700

    Split metal and lxc installation into separate recipes

--- test/cookbooks/lxctests/recipes/install_metal.rb
+++ test/cookbooks/lxctests/recipes/install_metal.rb
@@ -1,36 +1,4 @@
-# Ubuntu 12, not Ubuntu 14
-if node['platform'] == 'ubuntu' && node['platform_version'].to_i == 12
-  package 'python-software-properties' do
-    action :nothing
-  end.run_action(:install)
-end
-
-execute 'add-apt-repository ppa:ubuntu-lxc/daily' do
-  action :nothing
-end.run_action(:run)
-
-execute 'apt-get update' do
-  action :nothing
-end.run_action(:run)
-
-# Needed for Ubuntu 14, not Ubuntu 12
-if node['platform'] == 'ubuntu' && node['platform_version'].to_i == 14
-  package 'ruby1.9.1-dev' do
-    action :nothing
-  end.run_action(:upgrade)
-end
-
-package 'lxc' do
-  action :nothing
-end.run_action(:upgrade)
-
-package 'lxc-dev' do
-  action :nothing
-end.run_action(:upgrade)
-
-package 'lxc-templates' do
-  action :nothing
-end.run_action(:upgrade)
+include_recipe 'lxctests::install_lxc'
 
 chef_gem 'chef-metal' do
   source '/tmp/chef-metal-0.2.1.gem'
