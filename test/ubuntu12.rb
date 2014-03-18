80137e80b004e417f04643d038788c455cfb43ff
John Keiser <jkeiser@opscode.com>
2014-03-17 21:03:59 -0700

    Use vagrant mounting to install metal and metal-lxc from the host

--- test/ubuntu12.rb
+++ test/ubuntu12.rb
@@ -1,4 +1,7 @@
 # Use a Linux image
 vagrant_box 'precise64' do
-  url 'http://files.vagrantup.com/precise64.box' 
+  url 'http://files.vagrantup.com/precise64.box'
+  provisioner_options 'vagrant_config' => <<EOM
+    config.vm.synced_folder "../../..", "/mnt/host_src"
+EOM
 end
