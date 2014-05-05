8b950f990947d25c6eb5f9c7e125a9592a7e2129
Cristian Paul Peñaranda Rojas <paul@kristianpaul.org>
2014-05-04 22:47:47 -0500

    Update install_metal.rb
    No need for specific gem version anymore.

--- test/cookbooks/lxctests/recipes/install_metal.rb
+++ test/cookbooks/lxctests/recipes/install_metal.rb
@@ -1,11 +1,9 @@
 include_recipe 'lxctests::install_lxc'
 
 chef_gem 'chef-metal' do
-  source '/mnt/host_src/chef-metal/pkg/chef-metal-0.8.gem'
-  version '0.8'
+        action :install
 end
 
 chef_gem 'chef-metal-lxc' do
-  source '/mnt/host_src/chef-metal-lxc/pkg/chef-metal-lxc-0.3.gem'
-  version '0.3'
+        action :install
 end
